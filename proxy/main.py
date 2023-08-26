abd2c21a83520c78b43c788fcdb19e2aaceb9aa2
Ajacmac <jacob.macritchie@gmail.com>
2023-08-26 08:56:26 -0300

    Fixed event loop error

--- proxy/main.py
+++ proxy/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import sys
 
 LISTEN_PORT = 7500
 #DST_PORT = 26257
@@ -9,40 +10,45 @@ DST_HOST = "www.apache.org"
 
 # TODO: How do I account for the ?sslmode=verify-full
 # TODO: How do I specify that the connection to the server is TLS at all? 
-# Will I need to handle this differently between tests and the actual connectino to CockroachDB?
+# Will I need to handle this differently between tests and the actual connection to CockroachDB?
+
+# setup local root cert for CDB
+# certs, other parameters can be passed to open_connection() using an ssl context
+
+# check whether CockroachDB supports LISTEN/NOTIFY (also, figure out what that even is)
+
+async def pipe(reader, writer):
+    try:
+        while not reader.at_eof():
+            writer.write(await reader.read(2048))
+    finally:
+        writer.close()
 
 async def handle_client(client_reader, client_writer):
-    # read data from client
-    data = await client_reader.read(100)
-    print(f"Received {data.decode()!r}")
-
-    # connect to database server
-    server_reader, server_writer = await asyncio.open_connection(
-        DST_HOST, DST_PORT, ssl=True)
-
-    print('Sending to server...')
-    server_writer.write(data)
-    await server_writer.drain()
-
-    data = await server_reader.read(100)
-    print(f'Received: {data.decode()!r}')
-
-    # pass data back to client
-    client_writer.write(data)
-    await client_writer.drain()
-
-    # close connections
-    print('Close the connection')
-    server_writer.close()
-    await server_writer.wait_closed()
-
-    client_writer.close()
-    await client_writer.wait_closed()
+    try:
+        # connect to database server
+        server_reader, server_writer = await asyncio.open_connection(
+            DST_HOST, DST_PORT)
+        print("Connected to server")
+        pipe1 = pipe(client_reader, server_writer)
+        pipe2 = pipe(server_reader, client_writer)
+        await asyncio.gather(pipe1, pipe2)
+    except:
+        # printing the error message
+        print(f"Error connecting to server: {sys.exc_info()[0]}")
+    finally:
+        print("Closing connection")
+        client_writer.close()
 
 async def main():
     server = await asyncio.start_server(handle_client, '127.0.0.1', LISTEN_PORT)
 
-    async with server:
+    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    try:
         await server.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
 
 asyncio.run(main())
